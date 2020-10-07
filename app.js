23e838616f54b41a7d2dbf1321cc457f5db2b480
quannar178 <quannar178@gmail.com>
2020-10-07 16:24:58 +0700

    done oauth with google

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
-const connectDB = require('./config/db')
-const router = require('./routes/index')
+const connectDB = require('./config/db');
+const passport = require('passport');
+const session = require('express-session');
 //load config
 dotenv.config({ path: "./config/config.env" });
+
+//config passport
+require('./config/passport')(passport)
 
 //connect DB
 connectDB();
@@ -23,11 +27,23 @@ if(process.env.NODE_ENV === 'development'){
 app.engine('.hbs', exphbs({defaultLayout: 'main' ,extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
+//use session
+app.use(session({
+    secret: 'nguyen ba quan',
+    resave: false,
+    saveUninitialized: false
+}))
+
+//use passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 //use statid
 app.use(express.static(path.join(__dirname, 'public')));
 
 // use router
-app.use('/', router);
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
